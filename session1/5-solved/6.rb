9f20407fa670254691c89f6412b6a575c3c1568e
Dan Bolger <bowecymbal@gmail.com>
2015-04-20 16:25:40 +0100

    adding notes to examples to explain them still

--- session1/5-solved/6.rb
+++ session1/5-solved/6.rb
@@ -1,7 +1,7 @@
 def odds_and_evens(string, return_odds)
   to_return = "" 							# an empty string?
   string.size.times do |index|         		# string.size returns the length of the string  
-  											# then it's doing '.times' on that number, so it's doing the next thing ("|index|") however many number of times that 'times' got  
+ # then it's doing '.times' on that number, so it's doing the next thing ("|index|") however many number of times that 'times' got  
     next if return_odds && index.even?		# it's saying if return_odds is true, AND index.even is true
     next if !return_odds && index.odd?		# it's saying if 'not return_odds' is true AND index.odd? is true
     to_return << string[index]				# 
